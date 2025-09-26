02a2fdde945c8316a31813e7eaba8a9f3da5a6ee
vishal135679 <vishalkr135679@gmail.com>
2025-09-26 19:27:35 +0530

    Delete Harmony-Music-main directory

--- Harmony-Music-main/android/app/src/main/kotlin/com/anandnet/harmonymusic/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.anandnet.harmonymusic
-
-import com.ryanheise.audioservice.AudioServiceActivity
-
-class MainActivity : AudioServiceActivity() {
-}
--- Harmony-Music-main/lib/ui/widgets/playlist_album_scroll_behaviour.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class PlaylistAlbumScrollBehaviour extends MaterialScrollBehavior {
-   @override
-  Widget buildOverscrollIndicator(
-      BuildContext context, Widget child, ScrollableDetails details) {
-    return child;
-  }
-}
